65a5d6b0adab5e4d1ad148972f51b1b755c2e53e
Simon Owen <simon@simonowen.com>
2016-05-08 01:12:16 +0100

    Added explicit read support for SAM Coupe Pro-DOS .cpm
    Previously we relied on .raw reading recognising the file size,
    but that generates a warning. SAM images shouldn't be treated as
    2nd class citizens!

--- src/types/cpm.cpp
+++ src/types/cpm.cpp
@@ -3,6 +3,21 @@
 #include "SAMdisk.h"
 #include "types.h"
 
+bool ReadCPM (MemFile &file, std::shared_ptr<Disk> &disk)
+{
+	Format fmt = RegularFormat::ProDos;
+
+	// 720K images with a .cpm extension use the SAM Coupe Pro-Dos parameters
+	if (file.size() != fmt.disk_size() || !IsFileExt(file.name(), "cpm"))
+		return false;
+
+	file.rewind();
+	disk->format(fmt, file.data());
+	disk->strType = "Pro-DOS";
+
+	return true;
+}
+
 bool WriteCPM (FILE *f_, std::shared_ptr<Disk> &disk)
 {
 	return WriteRegularDisk(f_, *disk, RegularFormat::ProDos);
