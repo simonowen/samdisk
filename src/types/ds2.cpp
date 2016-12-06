eef52c50a57a5d166f4dfc4000ee677b0efe45a8
Simon Owen <simon@simonowen.com>
2016-12-06 21:10:40 +0000

    Fixed DS2 false-positive
    400K .dsk images were assumed to be split DSK/DS2 SAM Coupe images,
    and would fail when the companion .ds2 image was not found.
    
    The attempt to open the .ds2 image was also giving a confusing file
    not found error due to a thrown exception, before a more helpful
    error message could be displayed.
    
    We now require both .dsk and .ds2 images to exist and be the correct
    size before accepting them as DS2 files.

--- src/types/ds2.cpp
+++ src/types/ds2.cpp
@@ -7,8 +7,6 @@ bool ReadDS2 (MemFile &file, std::shared_ptr<Disk> &disk)
 {
 	std::string path = file.path();
 	MemFile file2;
-	MemFile *pfileHead0 = &file;
-	MemFile *pfileHead1 = &file2;
 
 	Format fmt { RegularFormat::MGT };
 
@@ -16,39 +14,29 @@ bool ReadDS2 (MemFile &file, std::shared_ptr<Disk> &disk)
 	if (!file.rewind() || file.size() != fmt.disk_size() / 2)
 		return false;
 
-	// If we've been given the main file, open the second side
-	if (IsFileExt(path, "dsk"))
-	{
-		// DSK->DS2
-		size_t offset = path.length() - 1;
-		path[offset] = '2';
-
-		if (!file2.open(path))
-			throw util::exception("missing .ds2 companion file");
-	}
-	// Or if given the data file, switch to the header
-	else if (IsFileExt(path, "ds2"))
-	{
-		// DS2->DSK (preserving case from 2nd char)
-		size_t offset = path.length() - 1;
-		path[offset] = 'K' | (path[offset - 1] & 0x20);
-
-		if (!file2.open(path))
-			throw util::exception("missing .dsk companion file");
-
-		std::swap(pfileHead0, pfileHead1);
-	}
-	// Reject other file extensions
-	else
+	// Require the supplied file be head 0
+	if (!IsFileExt(path, "dsk"))
 		return false;
+
+	// DSK->DS2
+	size_t offset = path.length() - 1;
+	path[offset] = '2';
+
+	// The DS2 file must also be present
+	try {
+		file2.open(path);
+	}
+	catch (...) {
+		return false;
+	}
 
 	// The companion file should also contain a single side
 	if (file2.size() != fmt.disk_size() / 2)
 		throw util::exception(path, " file size is incorrect");
 
 	// Join the sides
-	Data data(pfileHead0->data().begin(), pfileHead0->data().end());
-	data.insert(data.end(), pfileHead1->data().begin(), pfileHead1->data().end());
+	Data data(file.data().begin(), file.data().end());
+	data.insert(data.end(), file2.data().begin(), file2.data().end());
 
 	disk->format(fmt, data, true);
 	disk->strType = "DS2";
